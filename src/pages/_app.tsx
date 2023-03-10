58d611c46fbf56e6d5e1388e836fcdaedc1de92e
Yusufbek Alimatov <youbekalimatov@gmail.com>
2023-03-11 01:00:00 +0500

    install hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,6 +47,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Layout>
         {pageTransitions((styles, Component) => (
           <animated.div style={styles}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        id="hotjar-script"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3402710,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
