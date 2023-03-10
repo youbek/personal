95abd6027308347752f616edff93f5d18d97e7cf
Yusufbek Alimatov <youbekalimatov@gmail.com>
2023-03-11 01:09:05 +0500

    fix hotjar installation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        id="hotjar-script"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <body>
+        <Script id="hotjar-script" strategy="beforeInteractive">
+          {`          console.log('hotjar');
         (function(h,o,t,j,a,r){
           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
           h._hjSettings={hjid:3402710,hjsv:6};
@@ -17,11 +15,8 @@ export default function Document() {
           r=o.createElement('script');r.async=1;
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
           a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `,
-        }}
-      />
-      <body>
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
         <Main />
         <NextScript />
       </body>
