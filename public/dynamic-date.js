83f7b84d2d258e10598fa09951a0794016713068
Yusufbek Alimatov <youbekalimatov@gmail.com>
2026-03-04 13:44:38 +0500

    fix: dynamic date

--- public/dynamic-date.js
+++ public/dynamic-date.js
@@ -1,3 +1,6 @@
-const copyrightDate = document.getElementById("copyright-date");
+setTimeout(() => {
+  const copyrightDate = document.getElementById("copyright-date");
+  if (copyrightDate) return;
 
-copyrightDate.innerHTML = `© Yusufbek Alimatov ${new Date().getFullYear()}`;
+  copyrightDate.innerHTML = `© Yusufbek Alimatov ${new Date().getFullYear()}`;
+}, 0);
